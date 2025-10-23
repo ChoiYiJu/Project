3272d84a90bcdcdc47b9a0aecc5ed3eb7d022af1
ChoiYiJu <yijuju0621@gmail.com>
2025-10-23 09:57:12 +0900

    코드 추가

--- Assets/Project.cs
+++ Assets/Project.cs
@@ -7,7 +7,7 @@ public class Project : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("프로젝트를 시작합니다");
     }
 
     // Update is called once per frame
